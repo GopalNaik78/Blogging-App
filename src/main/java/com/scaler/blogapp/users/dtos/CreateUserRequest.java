3cf15ed495cd6bb0f7c284dc733b45e07e49c3fe
GopalNail <ajaynaik8971@gmail.com>
2024-04-23 12:53:06 +0530

    add services and there create update entities

---
+++ src/main/java/com/scaler/blogapp/users/dtos/CreateUserRequest.java
@@ -0,0 +1,17 @@
+package com.scaler.blogapp.users.dtos;
+
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.Setter;
+import org.springframework.lang.NonNull;
+
+@Data
+@Setter(AccessLevel.NONE)
+public class CreateUserRequest {
+    @NonNull
+    private String username;
+    @NonNull
+    private String password;
+    @NonNull
+    private String email;
+}
