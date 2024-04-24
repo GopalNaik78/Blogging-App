ba7bf10e3f80b0cf4de1601d5eee907527d5732d
GopalNail <ajaynaik8971@gmail.com>
2024-04-24 15:14:01 +0530

    add  security

--- src/main/java/com/scaler/blogapp/BlogappApplication.java
+++ src/main/java/com/scaler/blogapp/BlogappApplication.java
@@ -4,8 +4,8 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.password.PasswordEncoder;
 
 
 @SpringBootApplication
@@ -16,10 +16,10 @@ public class BlogappApplication {
         return new ModelMapper();
     }
 
-    @Bean
-    public PasswordEncoder passwordEncoder(){
-        return new BCryptPasswordEncoder();
-    }
+//    @Bean
+//    public PasswordEncoder passwordEncoder(){
+//        return new BCryptPasswordEncoder();
+//    }
     public static void main(String[] args) {
         SpringApplication.run(BlogappApplication.class, args);
     }
